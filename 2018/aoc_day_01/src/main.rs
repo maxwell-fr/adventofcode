1f7c958f52733db64381045d3fa67ad4ab327190
Maxwell I. French <maxwell@frenchnet.org>
2021-03-22 23:05:59 -0400

    Add solution to day 1 part 1.

---
+++ 2018/aoc_day_01/src/main.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::fs;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        2 => println!("Will use file: {}", args[1]),
+        _ => {
+            println!("Need one filename to open.");
+            return Ok(());
+        }
+    };
+
+    let problem_input:String = fs::read_to_string(&args[1])?;
+
+    part_1(&problem_input)?;
+
+    Ok(())
+}
+
+fn part_1(input: &String) -> Result<()>{
+    let mut accumulator = 0;
+
+    for ln in input.lines() {
+        accumulator += ln.parse::<i32>()?;
+    }
+
+    println!("Resulting frequency: {}", accumulator);
+
+    Ok(())
+}
