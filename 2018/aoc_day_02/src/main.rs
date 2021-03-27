69e3f127ed0c3d7efbb1bf231220db04ec070b22
Maxwell I. French <maxwell@frenchnet.org>
2021-03-26 21:27:40 -0400

    Add solution to day 2 part 2.

--- 2018/aoc_day_02/src/main.rs
+++ 2018/aoc_day_02/src/main.rs
@@ -1,10 +1,17 @@
 use std::collections::HashMap;
 use aoc_util::aoc_util::*;
+use std::time::Instant;
 
 fn main() -> AocResult<()> {
     let problem_input = get_problem_input()?;
     println!("Checksum: {}", part_1(&problem_input)?);
+    let start = Instant::now();
+    println!("A: ID differing by one character, common chars only: {}", part_2a(&problem_input)?);
+    let after_a = Instant::now();
+    println!("B: ID differing by one character, common chars only: {}", part_2b(&problem_input)?);
+    let after_b = Instant::now();
 
+    println!("2a: {:?}  2b: {:?}", after_a - start, after_b - after_a);
     Ok(())
 }
 
@@ -25,4 +32,52 @@ fn part_1(input: &String) -> AocResult<i32> {
     }
 
     Ok(three_count * two_count)
+}
+
+fn part_2a(input: &String) -> AocResult<String> {
+    for ln_a in input.lines() {
+        for ln_b in input.lines().skip(1) {
+            if ln_a.len() != ln_b.len() {
+                continue;
+            }
+
+            let mut common_c = String::with_capacity(ln_a.len());
+            ln_a.chars().zip(ln_b.chars()).filter(|(a,b)| a==b).for_each(|(a,_b)| common_c.push(a));
+            if common_c.len() == ln_a.len() - 1 {
+                return Ok(common_c);
+            }
+        }
+    }
+
+    Err("Nothing found.".into())
+}
+
+fn part_2b(input: &String) -> AocResult<String> {
+    for ln_a in input.lines() {
+        for ln_b in input.lines().skip(1) {
+            if ln_a.len() != ln_b.len() {
+                continue;
+            }
+
+            let mut common_c = String::with_capacity(ln_a.len());
+            let mut diff_count = 0;
+            for (a, b) in ln_a.chars().zip(ln_b.chars()){
+                if a != b {
+                    diff_count += 1;
+                    if diff_count > 1 {
+                        break;
+                    }
+                }
+                else {
+                    common_c.push(a);
+                }
+            }
+
+            if diff_count == 1 {
+                return Ok(common_c);
+            }
+        }
+    }
+
+    Err("Nothing found.".into())
 }
